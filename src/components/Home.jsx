701035d9406ae1ba5ef232b4e96760d57c571c00
daviazevdo <azvd.davi@gmail.com>
2022-07-07 17:46:12 -0300

    requisito 4

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = ({
+      categories: [],
+    });
+  }
+
+  componentDidMount() {
+    this.teste();
+  }
+
+  teste = async () => {
+    const response = await getCategories();
+    this.setState({
+      categories: response,
+    });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
       <div>
         <p data-testid="home-initial-message">
@@ -11,6 +31,18 @@ class Home extends Component {
         <Link data-testid="shopping-cart-button" to="/cart">
           Carrinho
         </Link>
+        <div className="categories">
+          { categories.map(({ id, name }) => (
+            <button
+              data-testid="category"
+              type="button"
+              value={ name }
+              key={ id }
+            >
+              { name }
+            </button>
+          )) }
+        </div>
       </div>
     );
   }
