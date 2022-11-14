99cab1658932dd2ed78ac2bf6ab55b4df92a9572
Hugodesousa <hugodesousa.16@gmail.com>
2022-11-14 16:21:15 -0300

    Updates

---
+++ precache-manifest.9ec692adc61308bcf3bba893e7d9d976.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91f2fc619a835c248cda2dfe5bfa8da8",
+    "url": "/frontend-online-store/index.html"
+  },
+  {
+    "revision": "8b686a188936d11aaeff",
+    "url": "/frontend-online-store/static/css/main.62f92392.chunk.css"
+  },
+  {
+    "revision": "e2ffef1b3c88bc970c47",
+    "url": "/frontend-online-store/static/js/2.6e245de7.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/frontend-online-store/static/js/2.6e245de7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b686a188936d11aaeff",
+    "url": "/frontend-online-store/static/js/main.9b0d37d7.chunk.js"
+  },
+  {
+    "revision": "582737c6385ae9ed11b6",
+    "url": "/frontend-online-store/static/js/runtime-main.720530ed.js"
+  },
+  {
+    "revision": "099154636c7d3ed386c2ec615ac5dde7",
+    "url": "/frontend-online-store/static/media/shopping-cart.09915463.svg"
+  },
+  {
+    "revision": "314a89939e2a3bd75c1ac85970ffeaaa",
+    "url": "/frontend-online-store/static/media/trybeLogo.314a8993.png"
+  }
+]);
