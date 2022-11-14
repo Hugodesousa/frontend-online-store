69001d932c3fcb96b968a09bca55584ac9d80397
Hugodesousa <hugodesousa.16@gmail.com>
2022-11-14 16:17:20 -0300

    css

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { getDetailProduct } from '../services/api';
 import addToCart from '../services/addToCart';
 import shopping from '../assets/shopping-cart.svg';
+import '../style/details.css';
 
 class ProductDetails extends Component {
   constructor({ match: { params } }) {
@@ -72,92 +73,105 @@ class ProductDetails extends Component {
     const { product, emailUser, evaluationText } = this.state;
     const { title, thumbnail, price } = product;
     return (
-      <div>
-        <Link data-testid="shopping-cart-button" to="/cart">
-          <img id="cart" alt="cart" src={ shopping } />
-        </Link>
-        <img className="product-image" src={ thumbnail } alt={ title } />
-        <h3 data-testid="product-detail-name" className="product-title">{title}</h3>
-        <p>
-          R$
-          {' '}
-          {price}
-        </p>
-        <button
-          type="button"
-          data-testid="product-detail-add-to-cart"
-          onClick={ () => addToCart(product) }
-        >
-          Adicionar ao carrinho
-        </button>
-
-        <div>
-          <label htmlFor="input-email">
-            <input
-              value={ emailUser }
-              name="emailUser"
-              onChange={ this.handleChange }
-              id="input-email"
-              type="email"
-              data-testid="product-detail-email"
-            />
-          </label>
-
-          <div onChange={ this.handleChange }>
-            1
-            <input
-              id="1"
-              type="radio"
-              data-testid="1-rating"
-            />
-
-            2
-            <input
-              id="2"
-              type="radio"
-              data-testid="2-rating"
-            />
-
-            3
-            <input
-              id="3"
-              type="radio"
-              data-testid="3-rating"
-            />
-
-            4
-            <input
-              id="4"
-              type="radio"
-              data-testid="4-rating"
-            />
-
-            5
-            <input
-              id="5"
-              type="radio"
-              data-testid="5-rating"
-            />
+      <div className="detailsContainer">
+        <div className="photoTile">
+          <img className="product-image" src={ thumbnail } alt={ title } />
+          <div className="titleDetails">
+            <h3 data-testid="product-detail-name" className="product-title">{title}</h3>
+            <Link data-testid="shopping-cart-button" to="/cart">
+              <img id="cart" alt="cart" src={ shopping } />
+            </Link>
+            <h4>
+              R$
+              {' '}
+              {price}
+            </h4>
+            <button
+              type="button"
+              data-testid="product-detail-add-to-cart"
+              onClick={ () => addToCart(product) }
+            >
+              Adicionar ao carrinho
+            </button>
           </div>
         </div>
+        <div className="formContainer">
+          <div>
+            <label htmlFor="input-email">
+              <input
+                value={ emailUser }
+                name="emailUser"
+                onChange={ this.handleChange }
+                id="input-email"
+                type="email"
+                data-testid="product-detail-email"
+                placeholder="email"
+                className="formItem"
+              />
+            </label>
 
-        <label htmlFor="evaluation">
-          <input
-            value={ evaluationText }
-            name="evaluationText"
-            onChange={ this.handleChange }
-            id="evaluation"
-            data-testid="product-detail-evaluation"
-            type="text"
-          />
-        </label>
-        <button
-          onClick={ this.publishReview }
-          type="button"
-          data-testid="submit-review-btn"
-        >
-          Enviar
-        </button>
+            <div
+              onChange={ this.handleChange }
+              className="formItem"
+            >
+              1
+              <input
+                id="1"
+                type="radio"
+                data-testid="1-rating"
+              />
+
+              2
+              <input
+                id="2"
+                type="radio"
+                data-testid="2-rating"
+              />
+
+              3
+              <input
+                id="3"
+                type="radio"
+                data-testid="3-rating"
+              />
+
+              4
+              <input
+                id="4"
+                type="radio"
+                data-testid="4-rating"
+              />
+
+              5
+              <input
+                id="5"
+                type="radio"
+                data-testid="5-rating"
+              />
+            </div>
+          </div>
+          <label htmlFor="evaluation">
+            <input
+              value={ evaluationText }
+              name="evaluationText"
+              onChange={ this.handleChange }
+              id="evaluation"
+              data-testid="product-detail-evaluation"
+              type="text"
+              placeholder="comentário"
+              className="formItem"
+            />
+          </label>
+          <button
+            onClick={ this.publishReview }
+            type="button"
+            data-testid="submit-review-btn"
+            className="formItem"
+          >
+            Enviar
+          </button>
+        </div>
+
 
         { this.verifyLocalStorage() }
       </div>
